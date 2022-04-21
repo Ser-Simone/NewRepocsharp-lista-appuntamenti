f52d8eefc2bc43153f0a65b09d7d4013d1ebd16d
Simone Serpi <simoneserpi@hotmail.it>
2022-04-21 17:21:33 +0200

    M

--- ListaAppuntamenti/Appuntamento.cs
+++ ListaAppuntamenti/Appuntamento.cs
@@ -8,5 +8,61 @@ namespace ListaAppuntamenti
 {
     internal class Appuntamento
     {
+
+        public DateTime dataAppuntamento;
+        public DateTime dataOdierna = new DateTime();
+        public string nome;
+        public string Cognome;
+        public string titoloAppuntamento;
+        public string luogoAppuntamento;
+
+
+        public Appuntamento(string nome, string Cognome, string titoloAppuntamento, string luogoAppuntamento, DateTime dataAppuntamento)
+        {
+            this.nome = nome;
+            this.Cognome = Cognome;
+            this.titoloAppuntamento = titoloAppuntamento;
+            this.luogoAppuntamento = luogoAppuntamento;
+            this.dataAppuntamento = dataAppuntamento;
+            this.dataOdierna = dataOdierna;
+            
+        }
+
+        
+        public DateTime setcambiaAppuntamento()
+        {
+            DateTime nuovaData = new DateTime();
+        }
+        public string getnome()
+        {
+            return nome;
+        }
+        public string getCognome()
+        {
+            return Cognome;
+        }
+        public string gettitoloAppuntamento()
+        {
+            return titoloAppuntamento;
+        }
+        public string getluogoAppuntamento()
+        {
+            return luogoAppuntamento;   
+        }
+        public DateTime getdataAppuntamento()
+        {
+            return dataAppuntamento;
+        }
+        
+        public virtual void ToString()
+        {
+            Console.WriteLine(nome);
+            Console.WriteLine(Cognome);
+            Console.WriteLine(titoloAppuntamento);
+            Console.WriteLine(luogoAppuntamento);
+            Console.WriteLine(dataAppuntamento);
+            
+        }
+        
     }
 }
--- ListaAppuntamenti/Program.cs
+++ ListaAppuntamenti/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ListaAppuntamenti;
+
+
+Console.WriteLine("Quanti appuntamenti vuoi inserire?");
+int numAppuntamenti = int.Parse(Console.ReadLine());
+
+
+
+List<Appuntamento> listaAppuntamenti = new List<Appuntamento>();
+
+for (int i = 0; i < numAppuntamenti; i++)
+{
+    Console.WriteLine("inserisci il tuo nome");
+    string nome = Console.ReadLine();
+    Console.WriteLine("inserisci il tuo cognome");
+    string cognome = Console.ReadLine();
+
+    Console.WriteLine("inserisci il titolo dell'appuntamento");
+   string titoloAppuntamento =  Console.ReadLine();
+
+    Console.WriteLine("inserisci il luogo dell'appuntamento");
+    string luogoAppuntamento = Console.ReadLine();
+
+    Console.WriteLine("inserisci la data dell'appuntamento");
+    string dataAppuntamento = Console.ReadLine();
+    DateTime dataInserita = DateTime.Parse(dataAppuntamento);
+
+    Appuntamento appuntamento1 = new Appuntamento(nome, cognome, titoloAppuntamento,luogoAppuntamento, dataInserita);
+    listaAppuntamenti.Add(appuntamento1);
+}
+
+foreach (Appuntamento appuntamento in listaAppuntamenti)
+{
+    appuntamento.ToString();
+}
+
+Console.WriteLine("Vuoi Cambiare un appuntamento?");
+string risposta = Console.ReadLine();
+
+
+if (risposta == "si")
+
+{
+    Appuntamento.cambiaAppuntamento()
+}
